2a86071cb184a0429a657dc84797ca3ef8db58d4
Bryan Moreno <bfourbryan@gmail.com>
2020-10-30 05:27:50 -0700

    Addition of pseudocoding to js file

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//DECLARE a list of 'numbers'
+
+
+// DECLARE a list of 'lowercaseLetters'
+
+// DECLARE a list of 'uppercaseLetters'
+
+// DECLARE a list of 'specialCharacters'
+
+
+function generatePassword() {
+  
+  // PROMPT the user for a 'passwordLength'
+
+  // CONFIRM if the password generator  'isUsingNumbers'
+
+  // CONFIRM if the password generator 'isUsingLowercaseLetters'
+
+  // CONFIRM if the password generator 'isUsingUppercaseLetters'
+
+  // CONFIRM if the password generator 'isUsingSpecialCharacters'
+
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
