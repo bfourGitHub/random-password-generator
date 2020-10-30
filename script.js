a2efa556e1e02a8e48c691ee10e9a97e2e8ac005
Bryan Moreno <bfourbryan@gmail.com>
2020-10-30 06:20:15 -0700

    Addition of more pseudocode for first solution of password generator

--- script.js
+++ script.js
@@ -14,15 +14,64 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   
   // PROMPT the user for a 'passwordLength'
+  var passwordLength = prompt("Provide a length");
+  // IF NOT ( passwordLength >= 8 || passwordLength <=128 )
+  if( !( passwordLength >= 8 || passwordLength <=128 )) {
+    // THEN alert to the user to provide a correct password length
+    alert("WRONG!")
+    // AND Exit Function
+    return;
+  }
 
+  //DECLARE a new list of 'charactersToUse'
+  var charactersToUse = [];
+  
+  // DECLARE a new 'password' string
+  var password = "";
+  
   // CONFIRM if the password generator  'isUsingNumbers'
+
+  // IF 'isUsingNumbers'
+  // THEN push 'numbers' into 'charactersToUse' list
+  // AND APPEND one random number from the 'numbers' list
 
   // CONFIRM if the password generator 'isUsingLowercaseLetters'
 
+  // IF 'isUsingLowercaseLetters'
+  // THEN push 'lowercaseLetters' into 'charactersToUse' list
+  
+
   // CONFIRM if the password generator 'isUsingUppercaseLetters'
+
+  // IF 'isUsingUppercaseLetters'
+  // THEN push 'uppercaseLetters' into 'charactersToUse' list
 
   // CONFIRM if the password generator 'isUsingSpecialCharacters'
 
+  // IF 'isUsingSpecialCharacters'
+  // THEN push 'specialCharacters' into 'charactersToUse' list
+
+  // IF NOT 'charactersToUse.length' **if the user selects no options**
+  //THEN ALERT to the user that they need to provide a correct length
+  //AND EXIT FUNCTION
+
+
+  // FOR i = 0; CONTINUE as long as i < passwordLength; i++;
+  for ( var i = 0; i < passwordLength; i++) {
+
+  }
+  // OR
+
+  // WHILE password.length < passwordLength
+  while( password.length < passwordLength ) {
+
+    // SELECT 'randomCharacter' from 'charactersToUse'
+    // APPEND 'randonCharacter' to 'password' string
+
+  }
+
+  //RETURN 'password'
+  return password;
 
 }
 
